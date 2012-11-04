0bcc9575638152f4da4cdbb84721f409d351b8e7
Sevak <sevak.gd@gmail.com>
2012-11-04 14:38:41 -0500

    divides hull into upper and lower

--- a4.cpp
+++ a4.cpp
@@ -7,12 +7,33 @@
 using namespace std;
 
 class plane{
+protected:
   //member variables
   vector< pair<int, int> > points; //contains points
   //member functions
 public:
   void populate(string fname);
 };
+
+class hull:public plane{
+public:
+  hull(){};
+  //member variables
+  pair<int, int> lext;
+  pair<int, int> rext;
+  //member functions
+  vector< pair<int, int> > hullpoints; //contains points
+  void divide();
+  void convexify(hull whole);
+};
+
+float determinant(pair<int, int> mtrx [3]){
+  float dtr; //result
+
+  dtr = (mtrx[0].first*mtrx[1].second) + (mtrx[1].first*mtrx[2].second) + (mtrx[2].first*mtrx[0].second) - (mtrx[2].first*mtrx[1].second) - (mtrx[1].first*mtrx[0].second) - (mtrx[0].first*mtrx[2].second);
+
+  return dtr;
+}
 
 void plane::populate(string fname){
 
@@ -33,22 +54,55 @@ void plane::populate(string fname){
   sort (points.begin(), points.end()); //overloaded by <utility>
 }
 
-float determinant(pair<int, int> mtrx [3]){
+void hull::divide(){
   
-  float dtr; //result
+  lext = points.front();
+  rext = points.back();
+  hull lower, upper;
 
-  dtr = (mtrx[0].first*mtrx[1].second) + (mtrx[1].first*mtrx[2].second) + (mtrx[2].first*mtrx[0].second) - (mtrx[2].first*mtrx[1].second) - (mtrx[1].first*mtrx[0].second) - (mtrx[0].first*mtrx[2].second);
+  int dtrm;
+  pair <int, int> mtrxsend [3] = {lext, rext, lext};
+  vector< pair<int, int> >::iterator it;
+  for (it=points.begin()+1; it<points.end()-1; ++it){
+    //cout << it->first << endl;
+    mtrxsend[2] = *it;
+    dtrm = determinant(mtrxsend);
+    if(dtrm<1){
+      lower.hullpoints.push_back(*it);}//lower hull
+    else if(dtrm>1){
+      upper.hullpoints.push_back(*it);}//upper hull
+  }
+}
 
-  return dtr;
+void hull::convexify(hull whole){
+
+  //base case: s1 = no elements; s2 = no elements
+
+  //for element in set
+    //find abs determinent
+    //divide by base length
+    //update max if greater than prev
+
+  //for element in upper
+    //find deter of p1pmax
+    //if deter > 1
+      //add to new vector
+    //find deter of pmaxp2
+    //if deter> 1
+      //add to s2 vector
+
+  //convexify(s1)
+  //convexify(s2)
 }
 
 int main(){
-  plane one;
+  hull one;
  
   string filename;
   cin >> filename;
   
   one.populate(filename);
+  one.divide();
   
   return 0;
 }
