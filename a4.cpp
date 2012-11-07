89353b761c1f429d75a65fd53a65e545917ca362
Sevak <sevak.gd@gmail.com>
2012-11-07 18:20:46 -0500

    back to prev version; added recursive convexify

--- a4.cpp
+++ a4.cpp
@@ -1,34 +1,7 @@
-#include <iostream>
-#include <fstream>
-#include <utility>
-#include <vector>
-#include <string>
-#include <algorithm>
-using namespace std;
+#include "a4.h"
 
-class plane{
-protected:
-  //member variables
-  vector< pair<int, int> > points; //contains points
-  //member functions
-public:
-  void populate(string fname);
-};
-
-class hull:public plane{
-public:
-  hull(){};
-  //member variables
-  pair<int, int> lext;
-  pair<int, int> rext;
-  //member functions
-  vector< pair<int, int> > hullpoints; //contains points
-  void divide();
-  void convexify(hull whole);
-};
-
-float determinant(pair<int, int> mtrx [3]){
-  float dtr; //result
+int determinant(pair<int, int> mtrx [3]){
+  int dtr; //result
 
   dtr = (mtrx[0].first*mtrx[1].second) + (mtrx[1].first*mtrx[2].second) + (mtrx[2].first*mtrx[0].second) - (mtrx[2].first*mtrx[1].second) - (mtrx[1].first*mtrx[0].second) - (mtrx[0].first*mtrx[2].second);
 
@@ -56,43 +29,115 @@ void plane::populate(string fname){
 
 void hull::divide(){
   
-  lext = points.front();
-  rext = points.back();
   hull lower, upper;
 
   int dtrm;
-  pair <int, int> mtrxsend [3] = {lext, rext, lext};
+  pair <int, int> mtrxsend [3] = {points.front(), points.back()};
+
+  convex.push_back(points.front());
+  convex.push_back(points.back());
+  
+  points.erase(points.begin());
+  points.pop_back();
+
   vector< pair<int, int> >::iterator it;
-  for (it=points.begin()+1; it<points.end()-1; ++it){
-    //cout << it->first << endl;
+  for (it=points.begin(); it<points.end(); ++it){
+
     mtrxsend[2] = *it;
     dtrm = determinant(mtrxsend);
-    if(dtrm<1){
-      lower.hullpoints.push_back(*it);}//lower hull
-    else if(dtrm>1){
-      upper.hullpoints.push_back(*it);}//upper hull
+
+    if(dtrm>0){
+      //cout << it->first << it->second << endl;
+
+      upper.points.push_back(*it);} //upper hull
+    else if(dtrm<0){
+      lower.points.push_back(*it);} //lower hull
+
   }
+  convexify(convex[0], convex[1], upper);
+  convexify(convex[0], convex[1], lower);
 }
 
-void hull::convexify(hull whole){
+pair <int, int> hull::findMax(pair <int, int> Ap, pair <int, int> Bp, hull suspects){
+  float dist;
+  float prev = 0;
+  int dtrm;
+  int baseLen = ((Bp.first-Ap.first)^2 + (Bp.second-Ap.second)^2)^(1/2);
 
-  //base case: s1 = no elements; s2 = no elements
+  pair <int, int> pointfar;
 
-  //for element in set
-    //find abs determinent
-    //divide by base length
-    //update max if greater than prev
+  pair <int, int> mtrxsend [3] = {Ap, Bp};
+  vector< pair <int, int> >::iterator it;
+  for (it = suspects.points.begin(); it< suspects.points.end(); ++it){
 
-  //for element in upper
-    //find deter of p1pmax
-    //if deter > 1
-      //add to new vector
-    //find deter of pmaxp2
-    //if deter> 1
-      //add to s2 vector
+    mtrxsend[2] = *it;
 
-  //convexify(s1)
-  //convexify(s2)
+    dtrm = determinant(mtrxsend);
+    if (dtrm < 0) {dtrm*=-1;};
+
+    dist = dtrm/baseLen;
+    if (dist >= prev){
+      pointfar = *it;
+      prev = dist;
+    };
+    
+  }
+
+  return pointfar;
+
+}
+
+void hull::convexify(pair <int, int> A, pair <int, int> B, hull toCheck){
+
+  if (toCheck.points.size() == 0){return;};
+
+
+  int dtrm;
+  hull nextCheck, nextCheckTwo;
+
+  pair <int, int> maxPoint = findMax(A, B, toCheck);
+  //cout << maxPoint.first << maxPoint.second << endl;
+
+  pair <int, int> mtrxsend [3] = {A, maxPoint};
+  vector< pair<int, int> >::iterator it;
+  for (it=toCheck.points.begin(); it<toCheck.points.end(); ++it){
+    if (*it==maxPoint){
+      convex.push_back(*it);
+      continue;
+    }
+    mtrxsend[2] = *it;
+    dtrm = determinant(mtrxsend);
+
+    if (maxPoint.second<A.second){
+      if(dtrm<=0){
+        nextCheck.points.push_back(*it);
+      }
+    }
+    else if (dtrm>=0)
+      nextCheck.points.push_back(*it);
+  }
+
+  pair <int, int> mtrxsendTwo [3] = {maxPoint, B};
+  vector< pair<int, int> >::iterator i;
+  for (i=toCheck.points.begin(); i<toCheck.points.end(); ++i){
+    if (*i==maxPoint){
+      //convex.push_back(*i);
+      continue;
+    }
+    mtrxsendTwo[2] = *i;
+    dtrm = determinant(mtrxsendTwo);
+    
+    if (maxPoint.second<A.second){
+      if(dtrm<=0){
+        nextCheckTwo.points.push_back(*it);
+      }
+    }
+    else if (dtrm>=0)
+      nextCheckTwo.points.push_back(*it);
+  }
+
+  convexify(A, maxPoint, nextCheck);
+  convexify(maxPoint, B, nextCheckTwo);    
 }
 
 int main(){
@@ -104,5 +149,9 @@ int main(){
   one.populate(filename);
   one.divide();
   
+  for (vector< pair <int, int> >::iterator i = one.convex.begin(); i != one.convex.end(); ++i)
+  {
+    cout << i->first << i->second << endl;
+  }
   return 0;
 }
